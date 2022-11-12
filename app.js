455f02579967500a3928bd3cc5bf04026b199b4d
Donovan <donovangriffin04@gmail.com>
2022-11-12 09:59:40 -0800

    added HTML JS and CSS

---
+++ app.js
@@ -0,0 +1,14 @@
+const timeAndMistakes = document.querySelector('#timeAndMistakes')
+
+window.addEventListener('load', load)
+
+
+function load(){
+  // console.log('hey')
+  // let time = 0
+  // setInterval(time++, 1000) 
+  // timeAndMistakes.innerText = `Time: ${time}`
+
+  
+}
+
