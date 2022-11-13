be7a092506a4b8ffb5323f49e2f13e8eeaaa039e
Donovan <donovangriffin04@gmail.com>
2022-11-13 11:32:58 -0800

    added logic to determine if a users input was incorrect or correct

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const timeAndMistakes = document.querySelector('#timeAndMistakes')
+const p = document.querySelector('#p1')
+const input = document.querySelector('#input')
 
-window.addEventListener('load', load)
 
+//api request to generate text
+axios.get('https://baconipsum.com/api/?type=meat-and-filler')
+.then(({ data }) => {
+  //promise to assign the random text to a p element
+  p.innerText = data[0]
+})
 
-function load(){
-  // console.log('hey')
-  // let time = 0
-  // setInterval(time++, 1000) 
-  // timeAndMistakes.innerText = `Time: ${time}`
+//variable to be updated if a letter is correct on incorrect
+let correct = 0 ;
+let wrong = 0
+
+//event listener assigned to input of text
+input.addEventListener('input', () => {
+  //variable to keep the users typing and the text in sync
+  let inputNumber = input.value.length - 1
 
   
-}
+  //variables to make getting the last character of string instead of the entire user input easier
+  let realInputValue = input.value.charAt(input.value.length - 1)
+  //keeps track of the letter of the text user is trying to type
+  let pLetter = p.innerText[inputNumber]
+
+//if statement which determines if the user is correct or incorrect and assigns points
+  if(realInputValue === pLetter){
+    p.style.color = 'green'
+      correct++
+
+  }else{
+    p.style.color = 'red'
+    wrong++
+  }
+
+})
 
